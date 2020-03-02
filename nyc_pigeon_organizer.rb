9a4ac216571c120c63b0f46e772b83fadbc52438
Josh Allen <joshmrallen@gmail.com>
2020-03-02 03:30:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   
   
-  
+  make_array(data)
   
   
 end
@@ -11,6 +11,12 @@ def make_array(hash_data)
   
   names_array = []
   
-  hash_data.each do ||
+  hash_data.each do |key,value|
+    key.each do |att, names|
+      names_array.push(names) 
+    end
+  end
+  
+  return names_array
   
 end
