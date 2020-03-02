d81c4c9032790f0f61904017e0e307d3a23655da
Josh Allen <joshmrallen@gmail.com>
2020-03-02 05:16:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -50,7 +50,9 @@ def make_hash(original_data, array_of_names)
       #compare with names_array to get a true or false value
       #if true, add that detail as a string to the array value of new_hash[name_key][attribute_name]
       
-      names.index
+      names.index do |name|
+        
+      end
       
     end #value.each end
     
